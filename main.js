5a6558253fa8a6dc70483918276d83e2b602bff5
Jessica-Ooi <113050980+Jessica-Ooi@users.noreply.github.com>
2023-01-04 23:13:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ weatherConditions.onload = function() {
         console.log(cObj);
         document.getElementById('location').innerHTML=cObj.name;
         document.getElementById('weather').innerHTML=cObj.weather[0].description;
-        document.getElementById('temerature').innerHTML=cObj.main.temp;
-        document.getElementById('desc').innerHTML="Wind Speed"+cObj.wind.speed;
+        document.getElementById('temperature').innerHTML=cObj.main.temp;
+        document.getElementById('desc').innerHTML="Wind Speed "+cObj.wind.speed;
 
 
     } //end if
@@ -40,39 +40,51 @@ weatherForecast.responseType = 'text';
 weatherForecast.send();
 
 weatherForecast.onload = function() {
-if (weatherForecast.status === 200){
-	fObj = JSON.parse(weatherForecast.responseText);
-	console.log(fObj);
-
-    var date_raw = fObj.list[0].dt_txt;
-    date_raw = date_raw.substring(5,11);
-	document.getElementById('r1c1').innerHTML=date_raw;
-    var iconcode = fObj.list[0].weather[0].icon;
-    var icon_path = "//openweathermap.org/img/w/"+iconcode+".png";
-    document.getElementById('r1c2').src = icon_path;
-    document.getElementById('r1c3').innerHTML = fObj.list[0].main.temp_min+"&deg";
-    document.getElementById('r1c4').innerHTML = fObj.list[0].main.temp_max+"&deg";
-
-
-    var date_raw = fObj.list[8].dt_txt;
-    date_raw = date_raw.substring(5,11);
-	document.getElementById('r2c1').innerHTML=date_raw;
-    var iconcode = fObj.list[8].weather[0].icon;
-    var icon_path = "//openweathermap.org/img/w/"+iconcode+".png";
-    document.getElementById('r2c2').src = icon_path;
-    document.getElementById('r2c3').innerHTML = fObj.list[8].main.temp_min+"&deg";
-    document.getElementById('r2c4').innerHTML = fObj.list[8].main.temp_max+"&deg";
-
-    var date_raw = fObj.list[16].dt_txt;
-    date_raw = date_raw.substring(5,11);
-	document.getElementById('r3c1').innerHTML=date_raw;
-    var iconcode = fObj.list[16].weather[0].icon;
-    var icon_path = "//openweathermap.org/img/w/"+iconcode+".png";
-    document.getElementById('r3c2').src = icon_path;
-    document.getElementById('r3c3').innerHTML = fObj.list[16].main.temp_min+"&deg";
-    document.getElementById('r3c4').innerHTML = fObj.list[16].main.temp_max+"&deg";
-
-} //end if
-}; //end function
-
-
+    if (weatherForecast.status === 200){
+        fObj = JSON.parse(weatherForecast.responseText);
+        console.log(fObj);
+    
+        var date_raw = fObj.list[0].dt_txt;
+        date_raw = date_raw.substring(5,11);
+        document.getElementById('r1c1').innerHTML= date_raw;
+    
+        var iconcode = fObj.list[0].weather[0].icon;
+        var icon_path = "//openweathermap.org/img/w/"+iconcode+".png";
+        document.getElementById('r1c2').src = icon_path;
+        document.getElementById('r1c3').innerHTML = fObj.list[0].main.temp_min+"&deg;";
+        document.getElementById('r1c4').innerHTML = fObj.list[0].main.temp_max+"&deg;";
+    
+        // Row 2
+        var date_raw = fObj.list[8].dt_txt;
+        date_raw = date_raw.substring(5,11);
+        document.getElementById('r2c1').innerHTML= date_raw;
+    
+        var iconcode = fObj.list[8].weather[0].icon;
+        var icon_path = "//openweathermap.org/img/w/"+iconcode+".png";
+        document.getElementById('r2c2').src = icon_path;
+        document.getElementById('r2c3').innerHTML = fObj.list[8].main.temp_min+"&deg;";
+        document.getElementById('r2c4').innerHTML = fObj.list[8].main.temp_max+"&deg;";
+    
+        // Row 3
+        var date_raw = fObj.list[16].dt_txt;
+        date_raw = date_raw.substring(5,11);
+        document.getElementById('r3c1').innerHTML= date_raw;
+    
+        var iconcode = fObj.list[12].weather[0].icon;
+        var icon_path = "//openweathermap.org/img/w/"+iconcode+".png";
+        document.getElementById('r3c2').src = icon_path;
+        document.getElementById('r3c3').innerHTML = fObj.list[16].main.temp_min+"&deg;";
+        document.getElementById('r3c4').innerHTML = fObj.list[16].main.temp_max+"&deg;";
+    
+        // Row 4 
+        var date_raw = fObj.list[24].dt_txt;
+        date_raw = date_raw.substring(5,11);
+        document.getElementById('r4c1').innerHTML= date_raw;
+    
+        var iconcode = fObj.list[24].weather[0].icon;
+        var icon_path = "//openweathermap.org/img/w/"+iconcode+".png";
+        document.getElementById('r4c2').src = icon_path;
+        document.getElementById('r4c3').innerHTML = fObj.list[24].main.temp_min+"&deg;";
+        document.getElementById('r4c4').innerHTML = fObj.list[24].main.temp_max+"&deg;";
+    }
+}
